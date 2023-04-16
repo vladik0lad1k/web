7f601b30f212f77189940ad2fb11ba4994da8fae
vladik0lad1k <porich2006@yandex.ru>
2023-04-16 13:36:37 +0300

    render templates

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from flask import Flask, request
-import sqlite3
+from flask import Flask, request, render_template
 from data import db_session
 from data.users import User
-
-import sqlalchemy as sa
-import sqlalchemy.orm as orm
-from sqlalchemy.orm import Session
+from data.news import News
 import sqlalchemy.ext.declarative as dec
 
 SqlAlchemyBase = dec.declarative_base()
 __factory = None
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
@@ -19,6 +15,7 @@ def index():
     db_sess = db_session.create_session()
     user = db_sess.query(User).first()
     return user.name
+
 
 @app.route('/user')
 def user():
@@ -35,6 +32,44 @@ def user():
     except Exception as e:
         return 'error'
     return 'ok'
+
+
+@app.route('/about')
+def about():
+    name = request.args.get("name")
+    if not name:
+        name = 'Друг'
+    return render_template('index.html', name=name)
+
+
+@app.route('/add-news')
+def add_news():
+    return render_template('add-news.html')
+
+
+@app.route('/save-news')
+def save_news():
+    title = request.args.get('title')
+    text = request.args.get('text')
+    try:
+        news = News()
+        news.title = title
+        news.content = text
+        db_sess = db_session.create_session()
+        db_sess.add(news)
+        db_sess.commit()
+    except Exception as e:
+        return 'error'
+    return render_template('add-news.html')
+
+@app.route('/news')
+def news():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).all()
+    return render_template('news.html', news=news)
+
+
+
 
 
 if __name__ == '__main__':
